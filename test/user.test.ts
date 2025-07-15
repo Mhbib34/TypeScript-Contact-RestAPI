84945e9c34ad4cb8ec31d3c106ba3cbd311f7995
Mhbib34 <mhabib34official@gmail.com>
2025-07-15 13:06:30 +0700

    feat : add register api

---
+++ test/user.test.ts
@@ -0,0 +1,51 @@
+import supertest from "supertest";
+import { web } from "../src/config/web";
+import { logger } from "../src/config/logging";
+import { UserTest } from "./test.util";
+
+describe("POST /api/users", () => {
+  afterEach(async () => {
+    await UserTest.deleteUser();
+  });
+
+  it("should create user", async () => {
+    const res = await supertest(web).post("/api/users").send({
+      username: "test",
+      password: "test123",
+      name: "test",
+    });
+    logger.debug(res.body);
+    console.log(res.body);
+    expect(res.status).toBe(201);
+    expect(res.body.data.username).toBe("test");
+    expect(res.body.data.name).toBe("test");
+  });
+
+  it("should cant create user if username already exists", async () => {
+    await supertest(web).post("/api/users").send({
+      username: "test",
+      password: "test123",
+      name: "test",
+    });
+    const res = await supertest(web).post("/api/users").send({
+      username: "test",
+      password: "test123",
+      name: "test",
+    });
+    logger.debug(res.body);
+    console.log(res.body);
+    expect(res.status).toBe(400);
+    expect(res.body.errors).toBe("Username already exists");
+  });
+
+  it("should cant create user if data is invalid", async () => {
+    const res = await supertest(web).post("/api/users").send({
+      username: "",
+      password: "",
+      name: "",
+    });
+    logger.debug(res.body);
+    console.log(res.body);
+    expect(res.status).toBe(400);
+  });
+});
